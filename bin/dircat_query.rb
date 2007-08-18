a051b3c74d8091171b16eff73e91c0ca0580530e
Giovanni Ferro <giovanni.ferro@gmail.com>
2007-08-18 09:24:23 +0000

    create le directory lib/dircat e test/dircat
    git-svn-id: svn+ssh://hestia.dreamhost.com/home/gferro_cm/svn.repo/projects/dircat/trunk@125 4aeedc48-2d20-0410-9d33-d2643f36798d

--- bin/dircat_query.rb
+++ bin/dircat_query.rb
@@ -1,8 +1,8 @@
-#!/usr/bin/env ruby 
+#!/usr/bin/env ruby
 
 $DIRCAT_HOME = File.expand_path( File.join( File.dirname( __FILE__), "..") )
 $:.unshift( File.join($DIRCAT_HOME, "lib" ) )
 
-require 'cli/dircat_query.rb'
+require 'dircat/cli/dircat_query.rb'
 
 DirCatQuery.run
---
+++ lib/dircat/cli/dircat_cfr.rb
@@ -0,0 +1,68 @@
+require 'optparse'
+require 'dircat/dircat.rb'
+
+#
+#
+#
+
+class DirCatCfr
+
+  def self.run
+    self.new.parse_args( ARGV )
+  end
+
+  def parse_args( args )
+
+    options = {}
+    opts = OptionParser.new
+    opts.banner =
+        "Usage: dircat_cfr.rb [options] <filedircat1> <filedircat2>\n" +
+        "fa la differenza fra il primo catalog e il secondo\n" +
+        "<filedircat1> - <filedircat2>\n" +
+        "e stampa sull'output con un formato"
+
+    opts.on("-h", "--help", "Print this message") do
+      puts opts
+      return
+    end
+
+    opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+      options[:verbose] = v
+    end
+
+    opts.on("-f FORMAT", "--fmt FORMAT", "formato") do |v|
+      options[:fmt] = v
+    end
+    rest = opts.parse( args )
+
+    # p options
+    # p ARGV
+
+    if rest.length < 2
+      puts "inserire il nome di due cataloghi da confrontare"
+      puts "dircat_cfr -h to print help"
+      exit
+    end
+
+    cat_filename1 = rest[0]
+    cat_filename2 = rest[1]
+
+    puts "build first set"
+    s1 = DirCat.loadfromfile(cat_filename1)
+
+    puts "build second set"
+    s2 = DirCat.loadfromfile(cat_filename2)
+
+    puts "build difference"
+    s3 = s1 - s2
+
+    case options[:fmt]
+    when "simple"
+      s3.fmt_simple
+    when "ruby"
+      s3.fmt_ruby( "." )
+    else
+      s3.fmt_simple
+    end
+  end
+end
