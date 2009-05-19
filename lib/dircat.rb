1f7d3c25e95951f6140e1d3a366e08e421718a46
Tokiro <tokiro.oyama@gmail.com>
2009-05-20 00:14:56 +0200

    convertito da svn a git aggiunto la possibilita' di creare una gem

---
+++ lib/dircat.rb
@@ -0,0 +1,3 @@
+class Dircat
+  VERSION = '0.0.1'
+end
--- test/test_dircat.rb
+++ test/test_dircat.rb
@@ -1,4 +1,4 @@
-$DIRCAT_HOME = File.expand_path( File.join( File.dirname( __FILE__), "..", "..") )
+$DIRCAT_HOME = File.expand_path( File.join( File.dirname( __FILE__), ".." ) )
 $:.unshift( File.join($DIRCAT_HOME, "lib" ) )
 $:.unshift( File.join($DIRCAT_HOME, "test" ) )
 
