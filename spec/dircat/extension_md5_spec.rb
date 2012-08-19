91e316c5928cb7f5940780498403a401165b22fb
Tokiro <tokiro.oyama@gmail.com>
2012-08-19 10:21:00 +0200

    move extension md5 and extension numeric to tree.rb project

--- spec/dircat/extension_md5_spec.rb
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-require File.expand_path( File.join(File.dirname(__FILE__), "..", "spec_helper") )
-describe "MD5" do
-  it "test_simple_md5" do
-    file_name = File.expand_path( File.join( File.dirname(__FILE__), "..", "..", "lib", "dircat", "extension_md5.rb" ) )
-    MD5.file( file_name ).to_s.should  ==  "ab4bc9cf679dede00672f2246e204b06"
-  end
-end
