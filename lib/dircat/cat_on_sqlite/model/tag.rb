b70fe912157ed468c829ad9ad299dae37b120525
Tokiro <tokiro.oyama@gmail.com>
2016-03-11 15:44:17 +0100

    updated to last active record

--- lib/dircat/cat_on_sqlite/model/tag.rb
+++ lib/dircat/cat_on_sqlite/model/tag.rb
@@ -42,11 +42,22 @@ module SimpleCataloger
     def self.match_category(tag_name)
       MATCHES.each do |re, category_name, convert|
         if re.match tag_name
-          category = Category.find_or_create_by_name(category_name)
-          tag      = Tag.find_or_create_by_name_and_category_id(
+
+          category = Category.find_by_name(category_name)
+          if category.nil?
+            category = Category.create(:name => category_name)
+          end
+
+          tag      = Tag.find_by_name_and_category_id(
               convert.call(tag_name),
               category.id
           )
+          if tag.nil?
+            tag      = Tag.create(
+              :name => convert.call(tag_name),
+              :category_id => category.id
+            )
+          end
           return category
         end
       end
